a7ca51449b7301e06607fb4ede77eefe664f3314
@SUMIT_KING <150422862+kajal99owner@users.noreply.github.com>
2025-02-11 03:59:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,12 +23,11 @@ YTDL_REGEX = (r"^((?:https?:)?\/\/)"
 s2tw = OpenCC('s2tw.json').convert
 
 
-@bot.on_message(filters.command("start"))
+@Jebot.on_message(filters.command("start"))
 async def start(client, message):
    if message.chat.type == 'private':
-       await Jebot.send_message(
-               chat_id=message.chat.id,
-               text="""<b>Hello! This is a YouTube Uploader Bot
+       await message.reply_text(
+               """<b>Hello! This is a YouTube Uploader Bot
 
 I can download video or audio from Youtube. Made by @TheTeleRoid 🇮🇳
 
@@ -47,12 +46,11 @@ Hit help button to find out more about how to use me</b>""",
             disable_web_page_preview=True,        
             parse_mode="html")
 
-@bot.on_message(filters.command("help"))
+@Jebot.on_message(filters.command("help"))
 async def help(client, message):
     if message.chat.type == 'private':   
-        await Jebot.send_message(
-               chat_id=message.chat.id,
-               text="""<b>YouTube Bot Help!
+        await message.reply_text(
+               """<b>YouTube Bot Help!
 
 Just send a Youtube url to download it in video or audio format!
 
@@ -65,18 +63,17 @@ Just send a Youtube url to download it in video or audio format!
                                             "🛑 Support 🛑", url="https://t.me/TeleRoid14"),
                                   ],[
                                         InlineKeyboardButton(
-                                            "About Meh👤", url="https://t.me/TheTeleRoid")
+                                            "About Meh👤", callback_data="about")
                                     ]]
                             ),        
             disable_web_page_preview=True,        
             parse_mode="html")
 
-@bot.on_message(filters.command("about"))
+@Jebot.on_message(filters.command("about"))
 async def about(client, message):
     if message.chat.type == 'private':   
-        await Jebot.send_message(
-               chat_id=message.chat.id,
-               text="""<b>About TeleRoid YouTube Bot!</b>
+        await message.reply_text(
+               """<b>About TeleRoid YouTube Bot!</b>
 
 <b>👨‍💻 Developer:</b> <a href="https://t.me/PredatorHackerzZ_bot">Predator 🇮🇳</a>
 
@@ -104,7 +101,7 @@ async def about(client, message):
 
 # https://docs.pyrogram.org/start/examples/bot_keyboards
 # Reply with inline keyboard
-@bot.on_message(filters.private
+@Jebot.on_message(filters.private
                    & filters.text
                    & ~filters.edited
                    & filters.regex(YTDL_REGEX))
@@ -129,8 +126,8 @@ async def ytdl_with_button(_, message: Message):
     )
 
 
-@bot.on_callback_query(filters.regex("^ytdl_audio$"))
-async def callback_query_ytdl_audio(_, callback_query):
+@Jebot.on_callback_query(filters.regex("^ytdl_audio$"))
+async def callback_query_ytdl_audio(client, callback_query):
     try:
         url = callback_query.message.reply_to_message.text
         ydl_opts = {
@@ -140,10 +137,10 @@ async def callback_query_ytdl_audio(_, callback_query):
         }
         with YoutubeDL(ydl_opts) as ydl:
             message = callback_query.message
+            await message.edit_text("**Downloading audio...**") # Edit the callback query message
             await message.reply_chat_action("typing")
             info_dict = ydl.extract_info(url, download=False)
             # download
-            await callback_query.edit_message_text("**Downloading audio...**")
             ydl.process_info(info_dict)
             # upload
             audio_file = ydl.prepare_filename(info_dict)
@@ -153,62 +150,61 @@ async def callback_query_ytdl_audio(_, callback_query):
                 await asyncio.sleep(3)
                 await message.reply_chat_action("upload_document")
             await message.reply_chat_action("cancel")
-            await message.delete()
+            await message.delete()  # Delete the downloading message
     except Exception as e:
-        await message.reply_text(e)
-    await callback_query.message.reply_to_message.delete()
-    await callback_query.message.delete()
-
-
+        await callback_query.message.reply_text(str(e)) # Reply to the message where the download was initiated
+    await callback_query.message.reply_to_message.delete() #Delete the original text message with URL.
+    await callback_query.message.delete() #Delete the Choose Download type message
+    
 if Config.AUDIO_THUMBNAIL == "No":
-   async def send_audio(message: Message, info_dict, audio_file):
-       basename = audio_file.rsplit(".", 1)[-2]
-       # .webm -> .weba
-       if info_dict['ext'] == 'webm':
-           audio_file_weba = basename + ".weba"
-           os.rename(audio_file, audio_file_weba)
-           audio_file = audio_file_weba
-       # thumbnail
-       thumbnail_url = info_dict['thumbnail']
-       thumbnail_file = basename + "." + \
-           get_file_extension_from_url(thumbnail_url)
-       # info (s2tw)
-       webpage_url = info_dict['webpage_url']
-       title = s2tw(info_dict['title'])
-       caption = f"<b><a href=\"{webpage_url}\">{title}</a></b>"
-       duration = int(float(info_dict['duration']))
-       performer = s2tw(info_dict['uploader'])
-       await message.reply_audio(audio_file, caption=caption, duration=duration,
+    async def send_audio(message: Message, info_dict, audio_file):
+        basename = audio_file.rsplit(".", 1)[-2]
+        # .webm -> .weba
+        if info_dict['ext'] == 'webm':
+            audio_file_weba = basename + ".weba"
+            os.rename(audio_file, audio_file_weba)
+            audio_file = audio_file_weba
+        # thumbnail
+        thumbnail_url = info_dict['thumbnail']
+        thumbnail_file = basename + "." + \
+            get_file_extension_from_url(thumbnail_url)
+        # info (s2tw)
+        webpage_url = info_dict['webpage_url']
+        title = s2tw(info_dict['title'])
+        caption = f"<b><a href=\"{webpage_url}\">{title}</a></b>"
+        duration = int(float(info_dict['duration']))
+        performer = s2tw(info_dict['uploader'])
+        await message.reply_audio(audio_file, caption=caption, duration=duration,
                               performer=performer, title=title,
                               parse_mode='HTML', thumb=thumbnail_file)
-       os.remove(audio_file)
-       os.remove(thumbnail_file)
+        os.remove(audio_file)
+        os.remove(thumbnail_file)
 
 else:
     async def send_audio(message: Message, info_dict, audio_file):
-       basename = audio_file.rsplit(".", 1)[-2]
-       # .webm -> .weba
-       if info_dict['ext'] == 'webm':
-           audio_file_weba = basename + ".weba"
-           os.rename(audio_file, audio_file_weba)
-           audio_file = audio_file_weba
-       # thumbnail
-       lol = Config.AUDIO_THUMBNAIL
-       thumbnail_file = wget.download(lol)
-       # info (s2tw)
-       webpage_url = info_dict['webpage_url']
-       title = s2tw(info_dict['title'])
-       caption = f"<b><a href=\"{webpage_url}\">{title}</a></b>"
-       duration = int(float(info_dict['duration']))
-       performer = s2tw(info_dict['uploader'])
-       await message.reply_audio(audio_file, caption=caption, duration=duration,
+        basename = audio_file.rsplit(".", 1)[-2]
+        # .webm -> .weba
+        if info_dict['ext'] == 'webm':
+            audio_file_weba = basename + ".weba"
+            os.rename(audio_file, audio_file_weba)
+            audio_file = audio_file_weba
+        # thumbnail
+        lol = Config.AUDIO_THUMBNAIL
+        thumbnail_file = wget.download(lol)
+        # info (s2tw)
+        webpage_url = info_dict['webpage_url']
+        title = s2tw(info_dict['title'])
+        caption = f"<b><a href=\"{webpage_url}\">{title}</a></b>"
+        duration = int(float(info_dict['duration']))
+        performer = s2tw(info_dict['uploader'])
+        await message.reply_audio(audio_file, caption=caption, duration=duration,
                               performer=performer, title=title,
                               parse_mode='HTML', thumb=thumbnail_file)
-       os.remove(audio_file)
-       os.remove(thumbnail_file)
+        os.remove(audio_file)
+        os.remove(thumbnail_file)
 
-@bot.on_callback_query(filters.regex("^ytdl_video$"))
-async def callback_query_ytdl_video(_, callback_query):
+@Jebot.on_callback_query(filters.regex("^ytdl_video$"))
+async def callback_query_ytdl_video(client, callback_query):
     try:
         # url = callback_query.message.text
         url = callback_query.message.reply_to_message.text
@@ -219,10 +215,10 @@ async def callback_query_ytdl_video(_, callback_query):
         }
         with YoutubeDL(ydl_opts) as ydl:
             message = callback_query.message
+            await message.edit_text("**Downloading video...**") # Edit the callback query message
             await message.reply_chat_action("typing")
             info_dict = ydl.extract_info(url, download=False)
             # download
-            await callback_query.edit_message_text("**Downloading video...**")
             ydl.process_info(info_dict)
             # upload
             video_file = ydl.prepare_filename(info_dict)
@@ -232,52 +228,52 @@ async def callback_query_ytdl_video(_, callback_query):
                 await asyncio.sleep(3)
                 await message.reply_chat_action("upload_document")
             await message.reply_chat_action("cancel")
-            await message.delete()
+            await message.delete() # Delete the downloading message
     except Exception as e:
-        await message.reply_text(e)
-    await callback_query.message.reply_to_message.delete()
-    await callback_query.message.delete()
+        await callback_query.message.reply_text(str(e)) # Reply to the message where the download was initiated
+    await callback_query.message.reply_to_message.delete() #Delete the original text message with URL.
+    await callback_query.message.delete() #Delete the Choose Download type message
 
 if Config.VIDEO_THUMBNAIL == "No":
-   async def send_video(message: Message, info_dict, video_file):
-      basename = video_file.rsplit(".", 1)[-2]
-      # thumbnail
-      thumbnail_url = info_dict['thumbnail']
-      thumbnail_file = basename + "." + \
-          get_file_extension_from_url(thumbnail_url)
-      # info (s2tw)
-      webpage_url = info_dict['webpage_url']
-      title = s2tw(info_dict['title'])
-      caption = f"<b><a href=\"{webpage_url}\">{title}</a></b>"
-      duration = int(float(info_dict['duration']))
-      width, height = get_resolution(info_dict)
-      await message.reply_video(
-          video_file, caption=caption, duration=duration,
-          width=width, height=height, parse_mode='HTML',
-          thumb=thumbnail_file)
+    async def send_video(message: Message, info_dict, video_file):
+        basename = video_file.rsplit(".", 1)[-2]
+        # thumbnail
+        thumbnail_url = info_dict['thumbnail']
+        thumbnail_file = basename + "." + \
+            get_file_extension_from_url(thumbnail_url)
+        # info (s2tw)
+        webpage_url = info_dict['webpage_url']
+        title = s2tw(info_dict['title'])
+        caption = f"<b><a href=\"{webpage_url}\">{title}</a></b>"
+        duration = int(float(info_dict['duration']))
+        width, height = get_resolution(info_dict)
+        await message.reply_video(
+            video_file, caption=caption, duration=duration,
+            width=width, height=height, parse_mode='HTML',
+            thumb=thumbnail_file)
 
-      os.remove(video_file)
-      os.remove(thumbnail_file)
+        os.remove(video_file)
+        os.remove(thumbnail_file)
 
 else:
-   async def send_video(message: Message, info_dict, video_file):
-      basename = video_file.rsplit(".", 1)[-2]
-      # thumbnail
-      lel = Config.VIDEO_THUMBNAIL
-      thumbnail_file = wget.download(lel)
-      # info (s2tw)
-      webpage_url = info_dict['webpage_url']
-      title = s2tw(info_dict['title'])
-      caption = f"<b><a href=\"{webpage_url}\">{title}</a></b>"
-      duration = int(float(info_dict['duration']))
-      width, height = get_resolution(info_dict)
-      await message.reply_video(
-          video_file, caption=caption, duration=duration,
-          width=width, height=height, parse_mode='HTML',
-          thumb=thumbnail_file)
+    async def send_video(message: Message, info_dict, video_file):
+        basename = video_file.rsplit(".", 1)[-2]
+        # thumbnail
+        lel = Config.VIDEO_THUMBNAIL
+        thumbnail_file = wget.download(lel)
+        # info (s2tw)
+        webpage_url = info_dict['webpage_url']
+        title = s2tw(info_dict['title'])
+        caption = f"<b><a href=\"{webpage_url}\">{title}</a></b>"
+        duration = int(float(info_dict['duration']))
+        width, height = get_resolution(info_dict)
+        await message.reply_video(
+            video_file, caption=caption, duration=duration,
+            width=width, height=height, parse_mode='HTML',
+            thumb=thumbnail_file)
 
-      os.remove(video_file)
-      os.remove(thumbnail_file)
+        os.remove(video_file)
+        os.remove(thumbnail_file)
 
 def get_file_extension_from_url(url):
     url_path = urlparse(url).path
@@ -308,18 +304,18 @@ def get_resolution(info_dict):
     return (width, height)
 
 
-@bot.on_callback_query()
-async def button(bot, update):
+@Jebot.on_callback_query()
+async def button(client, update):
       cb_data = update.data
-      if "help" in cb_data:
-        await update.message.delete()
-        await help(bot, update.message)
-      elif "about" in cb_data:
-        await update.message.delete()
-        await about(bot, update.message)
-      elif "start" in cb_data:
-        await update.message.delete()
-        await start(bot, update.message)
+      if cb_data == "help":
+          await update.message.delete()
+          await help(client, update.message)
+      elif cb_data == "about":
+          await update.message.delete()
+          await about(client, update.message)
+      elif cb_data == "start":
+          await update.message.delete()
+          await start(client, update.message)
 
 print(
     """
